e037623746fa3536b9b33136239faa90158bd984
Shashank <shashank.kaphle@gmail.com>
2021-07-15 22:43:24 +0545

    folder restructuring

---
+++ Minor_project/build/precache-manifest.a2b6d6042e75de2c0effb6f37028452f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc61d980cf9a32d0c4e30af0be9b270",
+    "url": "/Pathfinding-Algorithm-Visualiser/index.html"
+  },
+  {
+    "revision": "91c966f6d7864fa8131b",
+    "url": "/Pathfinding-Algorithm-Visualiser/static/css/main.a95f1800.chunk.css"
+  },
+  {
+    "revision": "35a7056fa6b4fdda5409",
+    "url": "/Pathfinding-Algorithm-Visualiser/static/js/2.76021f12.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Pathfinding-Algorithm-Visualiser/static/js/2.76021f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c966f6d7864fa8131b",
+    "url": "/Pathfinding-Algorithm-Visualiser/static/js/main.58789dc6.chunk.js"
+  },
+  {
+    "revision": "bf7e8f7c29939682f2b3",
+    "url": "/Pathfinding-Algorithm-Visualiser/static/js/runtime-main.142ec5b4.js"
+  }
+]);
