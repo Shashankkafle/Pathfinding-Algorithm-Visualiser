adf7acba41e5f809d30383b46e0f23db96ab6b94
Shashank <shashank.kaphle@gmail.com>
2024-02-11 22:55:04 +0545

    "redefine iterator"

--- src/Algorithms/Astar.jsx
+++ src/Algorithms/Astar.jsx
@@ -9,41 +9,41 @@ export function aStar(grid, startNode, finishNode) {
   while (unvisitedNodes.length > 0) {
     var cheapestIndex = 0,
       cheapestNode = unvisitedNodes[0];
-    for (var i = 0; i < unvisitedNodes.length; i++) {
-      if (unvisitedNodes[i].cost < cheapestNode.cost) {
-        cheapestIndex = i;
-      }
-    }
-    cheapestNode = unvisitedNodes[cheapestIndex];
-    if (cheapestNode.isFinish) {
-      visitedNodes.push(finishNode);
-      return visitedNodes;
-    }
-    unvisitedNodes.splice(cheapestIndex, 1);
-    visitedNodes.push(cheapestNode);
-    var unvisitedNeighbours = getUnvisitedNeighborsforAstar(cheapestNode, grid);
+    for (let i = 0; i < unvisitedNodes.length; i++) {
+		if (unvisitedNodes[i].cost < cheapestNode.cost) {
+			cheapestIndex = i;
+		}
+	}
+	cheapestNode = unvisitedNodes[cheapestIndex];
+	if (cheapestNode.isFinish) {
+		visitedNodes.push(finishNode);
+		return visitedNodes;
+	}
+	unvisitedNodes.splice(cheapestIndex, 1);
+	visitedNodes.push(cheapestNode);
+	var unvisitedNeighbours = getUnvisitedNeighborsforAstar(cheapestNode, grid);
 
-    for (var i = 0; i < unvisitedNeighbours.length; i++) {
-      if (!visitedNodes.includes(unvisitedNeighbours[i])) {
-        var tempdist = cheapestNode.distance + 1;
-        var newPath = false;
-        if (unvisitedNodes.includes(unvisitedNeighbours[i])) {
-          if (tempdist < unvisitedNeighbours[i].distance) {
-            unvisitedNeighbours[i].distance = tempdist;
-            newPath = true;
-          }
-        } else {
-          unvisitedNeighbours[i].distance = tempdist;
-          newPath = true;
-          unvisitedNodes.push(unvisitedNeighbours[i]);
-        }
-        if (newPath) {
-          findCost(unvisitedNeighbours[i], finishNode);
-          unvisitedNeighbours[i].previousNode = cheapestNode;
-        }
-        findCost(unvisitedNeighbours[i], finishNode);
-      }
-    }
+	for (let j = 0; j < unvisitedNeighbours.length; j++) {
+		if (!visitedNodes.includes(unvisitedNeighbours[j])) {
+			var tempdist = cheapestNode.distance + 1;
+			var newPath = false;
+			if (unvisitedNodes.includes(unvisitedNeighbours[j])) {
+				if (tempdist < unvisitedNeighbours[j].distance) {
+					unvisitedNeighbours[j].distance = tempdist;
+					newPath = true;
+				}
+			} else {
+				unvisitedNeighbours[j].distance = tempdist;
+				newPath = true;
+				unvisitedNodes.push(unvisitedNeighbours[j]);
+			}
+			if (newPath) {
+				findCost(unvisitedNeighbours[j], finishNode);
+				unvisitedNeighbours[j].previousNode = cheapestNode;
+			}
+			findCost(unvisitedNeighbours[j], finishNode);
+		}
+	}
   }
 
   return visitedNodes;
